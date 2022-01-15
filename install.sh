8cf35d8410ed89ab5fc55e0f345a279df1bd8ede
Zack D <zdcode@nym.hush.com>
2022-01-15 14:55:45 -0800

    lol

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -e
 # make sure git is installed
 hash git 2>&1 >/dev/null || { echo "You'll need Git to run this." >&2 ; exit 1 ; }
 
+
 # setup homeshick
 HOMESHICK="$HOME/.homesick/repos/homeshick"
 if [ ! -d $HOMESHICK ]; then
